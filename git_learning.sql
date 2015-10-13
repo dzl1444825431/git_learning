fe00a5d7ed3f4a559aa2862aa5b180a9f9b520b7
Dzl <1444825431@qq.com>
2015-10-13 23:33:56 +0800

    第一到第五章 log

--- git_learning.sql
+++ git_learning.sql
@@ -27,9 +27,27 @@ git config --global core.quotepath false 中文编码
 git config --global i18n.logOutputEncoding gbk
 git config --global i18n.commitEncoding gbk
 git config --system core.fileMode false 忽略文件权限的可执行位
+git config --system user.name "<name>"
+git config --system user.email "<name>"
+git config --system alias.<name> <sub-command>
+git config --system alias.st status
+git config --system alias.ci commit
+git config --system alias.co checkout
+git config --system alias.br branch
 
+
+git rev-parse --git-dir  显示版本库.git目录所在的位置
+git rev-parse --show-toplevel 显示工作区根目录
+git rev-parse --show-prefix 相对于工作区根目录的相对目录
+git rev-parse --show-cdup 显示从当前目录(cd) 后退 (up)到工作区的根的深度
+
+git config <section>.<key> <value>
 
 git --version
+
+git grep '工作区文件内容搜索'
+
+git log --pretty=fuller
 
 
 git help -w <sub-command> 查看命令帮助
@@ -44,6 +62,7 @@ git reset --hard
 
 git init
 
-
+.git目录 --》仓库，repository
+git_learning目录 -->工作区
 
 
