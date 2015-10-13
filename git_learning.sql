4eac59ee68c7be65c81209609c59917bb51e1908
dzl <1444825431@qq.com>
2015-10-13 17:58:49 +0800

    10-13 git code commit

---
+++ git_learning.sql
@@ -0,0 +1,49 @@
+git add <file>
+git add -A
+
+git commit -m "<desc>"
+git commit -a
+git commit --amend 修改备注
+
+git status 
+git status -s
+
+git tag
+git tag <name> 新建一个里程碑
+
+
+git push
+git pull
+
+
+git stash        进度
+git stash pop
+
+git checkout <branch>
+
+git config --global core.pager 'less -+$LESS -FRX'
+git config --global color.ui true配置变量启用
+git config --global core.quotepath false 中文编码
+git config --global i18n.logOutputEncoding gbk
+git config --global i18n.commitEncoding gbk
+git config --system core.fileMode false 忽略文件权限的可执行位
+
+
+git --version
+
+
+git help -w <sub-command> 查看命令帮助
+
+git fetch 更新
+git clean -fdx
+git reset --hard
+
+增删改 撤销 对比 保存进度 
+配置设置
+
+
+git init
+
+
+
+
